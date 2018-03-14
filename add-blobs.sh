5cafdc850a2352eaf08d1a6f99396380071079ed
Toshiaki Maki <tmaki@pivotal.io>
2018-03-15 01:21:02 +0900

    Add logstatsh package

---
+++ add-blobs.sh
@@ -0,0 +1,22 @@
+
+#!/bin/sh
+
+DIR=`pwd`
+
+mkdir -p .downloads
+
+cd .downloads
+
+
+
+if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_162.tar.gz ];then
+    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_162.tar.gz
+    bosh add-blob --dir=${DIR} openjdk-1.8.0_162.tar.gz java/openjdk-1.8.0_162.tar.gz
+fi
+
+if [ ! -f ${DIR}/blobs/logstash/logstash-6.2.2.tar.gz ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/logstash/logstash-6.2.2.tar.gz
+    bosh add-blob --dir=${DIR} logstash-6.2.2.tar.gz logstash/logstash-6.2.2.tar.gz
+fi
+
+cd -
