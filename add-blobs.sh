a7aeaa926dc5f76b0dc66adf82f4e1ea534079c1
Toshiaki Maki <tmaki@pivotal.io>
2018-08-30 11:20:05 +0900

    Bump to Logstash 6.4.0

--- add-blobs.sh
+++ add-blobs.sh
@@ -14,9 +14,11 @@ if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_172.tar.gz ];then
     bosh add-blob --dir=${DIR} openjdk-1.8.0_172.tar.gz java/openjdk-1.8.0_172.tar.gz
 fi
 
-if [ ! -f ${DIR}/blobs/logstash/logstash-6.3.2.tar.gz ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/logstash/logstash-6.3.2.tar.gz
-    bosh add-blob --dir=${DIR} logstash-6.3.2.tar.gz logstash/logstash-6.3.2.tar.gz
+LOGSTASH_VERSION=6.4.0
+
+if [ ! -f ${DIR}/blobs/logstash/logstash-${LOGSTASH_VERSION}.tar.gz ];then
+    curl -L -O -J https://artifacts.elastic.co/downloads/logstash/logstash-${LOGSTASH_VERSION}.tar.gz
+    bosh add-blob --dir=${DIR} logstash-${LOGSTASH_VERSION}.tar.gz logstash/logstash-${LOGSTASH_VERSION}.tar.gz
 fi
 
 cd -
