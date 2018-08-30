d7b79574e917392f3c29551e7f53e98b35a79d7f
Toshiaki Maki <tmaki@pivotal.io>
2018-08-30 11:29:23 +0900

    Exclude java job

--- add-blobs.sh
+++ add-blobs.sh
@@ -8,12 +8,6 @@ mkdir -p .downloads
 cd .downloads
 
 
-
-if [ ! -f ${DIR}/blobs/java/openjdk-1.8.0_172.tar.gz ];then
-    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-1.8.0_172.tar.gz
-    bosh add-blob --dir=${DIR} openjdk-1.8.0_172.tar.gz java/openjdk-1.8.0_172.tar.gz
-fi
-
 LOGSTASH_VERSION=6.4.0
 
 if [ ! -f ${DIR}/blobs/logstash/logstash-${LOGSTASH_VERSION}.tar.gz ];then
