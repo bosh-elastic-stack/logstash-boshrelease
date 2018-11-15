5dbc24938dadb22e3efd1ff009f66d6eef3c5daf
Toshiaki Maki <tmaki@pivotal.io>
2018-11-15 20:42:30 +0900

    Add python package explicitly so that it works on Xenial stemcell

--- add-blobs.sh
+++ add-blobs.sh
@@ -7,12 +7,20 @@ mkdir -p .downloads
 
 cd .downloads
 
+blob_download() {
+  set -eu
+  local package=$1
+  local url=$2
+  local f=$3
+  if [ ! -f ${DIR}/blobs/${package}/${f} ];then
+    curl -L -J ${url} -o ${f}
+    bosh add-blob --dir=${DIR} ${f} ${package}/${f}
+  fi
+}
 
 LOGSTASH_VERSION=6.5.0
 
-if [ ! -f ${DIR}/blobs/logstash/logstash-${LOGSTASH_VERSION}.tar.gz ];then
-    curl -L -O -J https://artifacts.elastic.co/downloads/logstash/logstash-${LOGSTASH_VERSION}.tar.gz
-    bosh add-blob --dir=${DIR} logstash-${LOGSTASH_VERSION}.tar.gz logstash/logstash-${LOGSTASH_VERSION}.tar.gz
-fi
+blob_download logstash https://artifacts.elastic.co/downloads/logstash/logstash-${LOGSTASH_VERSION}.tar.gz logstash-${LOGSTASH_VERSION}.tar.gz
+blob_download python2.7 https://www.python.org/ftp/python/2.7.15/Python-2.7.15.tgz Python-2.7.15.tgz
 
 cd -
