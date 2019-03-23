283fc7fdad14618fa96bc7af6663dcf0048f2124
Toshiaki Maki <tmaki@pivotal.io>
2019-03-23 18:30:37 +0900

    Make heap size configurable

--- jobs/logstash/templates/config/env.sh
+++ jobs/logstash/templates/config/env.sh
@@ -8,6 +8,11 @@ export ES_USERNAME="<%= username %>"
 <% if_p('elasticsearch.password') do |password| %>
 export ES_PASSWORD="<%= password %>"
 <% end %>
+JVM_HEAP_SIZE=$((( $( cat /proc/meminfo | grep MemTotal | awk '{ print $2 }' ) * <%= p("logstash.jvm.heap_size_pct") %> ) / 100 / 1024 ))m
+<% if_p('logstash.heap_size') do |heap_size| %>
+JVM_HEAP_SIZE=<%= heap_size %>
+<% end %>
+export LS_JAVA_OPTS="-Xms$JVM_HEAP_SIZE -Xmx$JVM_HEAP_SIZE"
 <% p("logstash.env", {}).each do | k, v | %>
 export <%= k %>="<%= v %>"
 <% end %>
